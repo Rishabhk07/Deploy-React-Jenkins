f9ac2f40a61cee565ae5f24a6aea1cd7dbdc2a58
Rishabh Khanna <rishabhkhanna777@gmail.com>
2021-01-01 13:41:19 -0800

    initial commit for Deploy

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Hello World, From the World of Rishabhk07</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Please Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
